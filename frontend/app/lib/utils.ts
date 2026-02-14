84ae3663ef2460706d4b547716c2779bce45cf09
samuellupica <samuel.lupica@students.fhnw.ch>
2026-02-14 19:59:45 +0100

    fix: add break

--- frontend/app/lib/utils.ts
+++ frontend/app/lib/utils.ts
@@ -1,25 +1,26 @@
-import type { Link } from "~/sanity/types";
+import type { Link } from '~/sanity/types';
 
 export function linkResolver(link: Link | undefined) {
-  if (!link) return null;
+    if (!link) return null;
 
-  // If linkType is not set but href is, lets set linkType to "href".  This comes into play when pasting links into the portable text editor because a link type is not assumed.
-  if (!link.linkType && link.href) {
-    link.linkType = "href";
-  }
+    // If linkType is not set but href is, lets set linkType to "href".  This comes into play when pasting links into the portable text editor because a link type is not assumed.
+    if (!link.linkType && link.href) {
+        link.linkType = 'href';
+    }
 
-  switch (link.linkType) {
-    case "href":
-      return link.href || null;
-    case "page":
-      if (link?.page && typeof link.page === "string") {
-        return `/${link.page}`;
-      }
-    case "post":
-      if (link?.post && typeof link.post === "string") {
-        return `/posts/${link.post}`;
-      }
-    default:
-      return null;
-  }
+    switch (link.linkType) {
+        case 'href':
+            return link.href || null;
+        case 'page':
+            if (link?.page && typeof link.page === 'string') {
+                return `/${link.page}`;
+            }
+            break;
+        case 'post':
+            if (link?.post && typeof link.post === 'string') {
+                return `/posts/${link.post}`;
+            }
+        default:
+            return null;
+    }
 }
